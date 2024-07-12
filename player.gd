45d0d92225fa9968250286563a3af03677848ec5
SunnySatellites <AVeryDrunkPanda@gmail.com>
2024-07-12 01:52:36 -0500

    initial commit for jam just has basic player controller and p. much nothing else

---
+++ player.gd
@@ -0,0 +1,86 @@
+extends CharacterBody3D
+class_name Player
+
+#Onready Variables
+@onready var state_chart: StateChart = $StateChart
+@onready var camera_mount: Node3D = $CameraMount
+
+
+#proj stuff variables that are important to general function
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var deltaSuper : float = Engine.get_main_loop().root.get_physics_process_delta_time()
+
+@export var player_speed: float = 10.0
+@export var jump_velocity: float = 7.0
+
+#mouse mode stuff
+var mouse_motion := Vector2.ZERO
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	handle_state()
+	restartandexit()
+	handle_camera_rotation()
+	deltaSuper = get_physics_process_delta_time()
+
+func walking() -> void:
+	var input_dir := Input.get_vector("Left","Right","Forward","Backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = lerp(velocity.x, direction.x * player_speed, player_speed * deltaSuper * .5)
+		velocity.z = lerp(velocity.z, direction.z * player_speed, player_speed * deltaSuper * .5)
+	else:
+		velocity.x = move_toward(velocity.x, 0, player_speed * deltaSuper)
+		velocity.z = move_toward(velocity.z, 0, player_speed * deltaSuper)
+
+func restartandexit() -> void:
+	if Input.is_action_just_pressed("ui_end"):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+
+func falling() -> void:
+	velocity.y -=  gravity * deltaSuper
+
+func jumping() -> void:
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = jump_velocity
+
+func handle_state() -> void:
+	if not Input.is_anything_pressed() and is_on_floor():
+		state_chart.send_event("idle")
+	if is_on_floor() and Input.is_anything_pressed():
+		state_chart.send_event("ground")
+	if not is_on_floor():
+		state_chart.send_event("air")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			mouse_motion = -event.relative * 0.001
+
+func handle_camera_rotation() -> void:
+	rotate_y(mouse_motion.x)
+	camera_mount.rotate_x(mouse_motion.y)
+	camera_mount.rotation_degrees.x = clampf(
+		camera_mount.rotation_degrees.x, -90.0, 90.0
+	)
+	mouse_motion = Vector2.ZERO
+
+func _on_grounded_state_physics_processing(delta: float) -> void:
+	walking()
+	jumping()
+
+func _on_in_air_state_physics_processing(delta: float) -> void:
+	falling()
+
+func _on_idle_state_physics_processing(delta: float) -> void:
+	falling()
+	walking()
+#the
